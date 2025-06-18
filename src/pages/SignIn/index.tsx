532cad5d153ee5bdd2fd7f6e37e7a30f92f1a246
Willian Santos <willian.s@develops.today>
2025-06-18 11:40:36 -0300

    feat: added style to SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,49 @@
-import { Text, View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { styles } from './styles';
+
+import { StackParamList } from '@/routes/types';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 const SignIn = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
+
   return (
-    <View>
-      <Text>Tela de Login</Text>
+    <View className="flex-1 bg-[#F0F4FF]">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        enabled
+        className="flex-1 items-center justify-center">
+        <Image className="mb-[25px]" source={require('../../assets/Logo.png')} />
+
+        <View className={styles.areaInput}>
+          <TextInput className={styles.input} placeholder="Seu e-mail" />
+        </View>
+
+        <View className={styles.areaInput}>
+          <TextInput className={styles.input} placeholder="Sua senha" />
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          className="mt-[10px] h-[45px] w-[90%] items-center justify-center rounded-[8px] bg-[#3b3dbf]">
+          <Text className="text-[20px] text-[#FFF]">Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('SignUp')}
+          className="mb-[10px] mt-[10px]">
+          <Text>Criar uma conta!</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 };
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,13 +2,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SignIn from '@/pages/SignIn';
 import SignUp from '@/pages/SignUp';
+import { StackParamList } from './types';
 
-const AuthStack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator<StackParamList>();
 
 const AuthRoutes = () => {
   return (
     <AuthStack.Navigator>
-      <AuthStack.Screen name="SignIn" component={SignIn} />
+      <AuthStack.Screen options={{ headerShown: false }} name="SignIn" component={SignIn} />
       <AuthStack.Screen name="SignUp" component={SignUp} />
     </AuthStack.Navigator>
   );
